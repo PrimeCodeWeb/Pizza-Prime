e7712a48e438da4fe317e5943815d134e58c4a48
Nicolas Gabriel <ricardosantosssmakeup95@gmail.com>
2026-08-07 21:52:42 -0300

    corrige conflitos e arquivos do projeto

--- cardapio.js
+++ cardapio.js
@@ -1,18 +1,16 @@
-<<<<<<< HEAD
-document.addEventListener("DOMContentLoaded", () => {
-  const menu = document.querySelector(".botoes");
-  const botao = document.querySelector(".menu-hamburguer");
 
-  botao.addEventListener("click", () => {
-    menu.classList.toggle("ativo");
-  });
-=======
 document.addEventListener("DOMContentLoaded", () => {
-  const menu = document.querySelector(".botoes");
-  const botao = document.querySelector(".menu-hamburguer");
 
-  botao.addEventListener("click", () => {
-    menu.classList.toggle("ativo");
-  });
->>>>>>> 5622c1b27440bbb313984163b5f2a09f86584e72
+    const menu = document.querySelector(".botoes");
+    const botao = document.querySelector(".menu-hamburguer");
+
+    if (botao && menu) {
+
+        botao.addEventListener("click", () => {
+            menu.classList.toggle("ativo");
+        });
+
+    }
+
 });
+
